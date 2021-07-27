e5270e522324383f6068325c14e985802275c144
Grigoriy <vnukovg@gmail.com>
2021-07-27 21:10:32 +0300

    finish app

---
+++ src/components/Characters.js
@@ -0,0 +1,52 @@
+import character from '../store/character';
+import { observer } from 'mobx-react-lite';
+import ReactTooltip from "react-tooltip";
+import './characters.css';
+
+const Characters = observer(() => {
+  const findCharacters = (e) => {
+    character.setName(e.target.value);
+    character.filterCharacters();
+  }
+
+  const characters = character.characters.map((c) => {
+    return (
+      <div key={c.id} className="card">
+        <img src={c.image} alt="Logo" data-tip data-for={'registerTip-' + c.id} />
+        <ReactTooltip 
+          id={'registerTip-' + c.id} 
+          place="bottom" 
+          effect="solid" 
+          multiline={true}
+          className='card-tooltip'
+          arrowColor="white"
+          >
+            Name: {c.name}
+            <br/>
+            Status: {c.status}
+            <br/>
+            Species: {c.species}
+        </ReactTooltip>
+      </div>
+    )
+  })
+
+  return (
+    <div className="characters-block">
+      <div className="input-wrap">
+        <input
+          type="text"
+          className="input"
+          value={character.name}
+          onChange={findCharacters}
+        />
+      </div>
+
+      <div className="cards-wrap">
+        {characters}
+      </div>
+    </div>
+  )
+})
+
+export default Characters;
---
+++ src/store/character.js
@@ -0,0 +1,53 @@
+import { makeAutoObservable } from "mobx";
+import axios from 'axios';
+
+class Character {
+  characters = [];
+  name = '';
+  millisecondsPassed = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+    this.getCharacters();
+    setInterval(() => {
+      this.increaseTimer()
+    }, 500);
+  }
+
+  filterCharacters() {
+    if (this.name.length !== 1 && this.millisecondsPassed >= 500) {
+      this.resetTimer();
+      this.getCharacters();
+    }
+  }
+
+  getCharacters() {
+    const name = this.name.toLowerCase();
+
+    axios.get(`https://rickandmortyapi.com/api/character?name=${name}`)
+      .then(res => {
+        this.setCharacters(res.data.results);
+      })
+      .catch((e) => {
+        this.setCharacters([]);
+      })
+  }
+
+  setCharacters(results) {
+    this.characters = results;
+  }
+
+  setName(value) {
+    this.name = value;
+  }
+
+  increaseTimer() {
+    this.millisecondsPassed += 500;
+  }
+
+  resetTimer() {
+    this.millisecondsPassed = 0;
+  }
+}
+
+export default new Character();
